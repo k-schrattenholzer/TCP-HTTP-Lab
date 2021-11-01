484b488d9123c6054a468b2d721f578856e41e77
‘Katie <ktschr11@gmail.com>
2021-11-01 09:15:05 -0700

    passing echo test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,10 +2,29 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('app routes', () => {
+
   it('gets message from /', async() => {
     const response = await request(app).get('/');
 
     expect(response.text).toEqual('helloo');
   });
+
+  it('gets echo of response body from /echo', async() => {
+    const response = await request(app)
+      .post('/echo')
+      .send('helloo');
+
+    expect(response.text).toEqual('helloo');
+  });
+
+  // it('gets echo of response body from /echo', async() => {
+  //   const response = await request(app)
+  //     .post('/echo')
+  //     .send('helloo');
+
+  //   expect(response.text).toEqual('helloo');
+  // });
+
+////....////new tests above this line////....////
 });
-//dummy test passing
+
